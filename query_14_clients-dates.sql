ab3da7ad237a12b687e1980f31f033034b3e7957
Beastoloch <54348523+Beastoloch@users.noreply.github.com>
2023-04-26 23:22:07 +0700

    Delete query_14_clients-dates.sql

--- query_14_clients-dates.sql
+++
@@ -1,4 +0,0 @@
-SELECT H.Name, R.Room_number, Arrival_date, Departature_date
-FROM Client_base JOIN Room R ON R.Room_ID = Client_base.Room_ID JOIN Hotel H ON 
-R.Hotel_ID = H.Hotel_ID
-WHERE Client_base_ID = ?1
