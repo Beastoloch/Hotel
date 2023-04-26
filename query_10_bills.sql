a480526898d9fdbd79af367fcada46f4fcb023c0
Beastoloch <54348523+Beastoloch@users.noreply.github.com>
2023-04-26 23:21:24 +0700

    Delete query_10_bills.sql

--- query_10_bills.sql
+++
@@ -1,8 +0,0 @@
-SELECT * FROM Bill JOIN (
- SELECT Client_base_ID, Arrival_date
- FROM Client_base
- JOIN Room R ON R.Room_ID = Client_base.Room_ID
- WHERE R.Room_ID = ?1
- AND Departature_date > SYSDATE
-) This_visitor ON Bill.Client_base_ID = This_visitor.Client_base_ID
-WHERE Bill_Date > Arrival_date
